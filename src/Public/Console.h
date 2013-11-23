8db8b7868d156c020d1a23e9ad0ccb8d75ae7dee
zc5872061 <287211409@qq.com>
2013-11-23 18:07:26 +0800

    增加mysql++,整体结构修改

--- src/Public/Console.h
+++
@@ -1,6 +0,0 @@
-#ifndef _CONSOLE_H
-#define _CONSOLE_H
-
-#include "WinConsole.h"
-
-#endif // _CONSOLE_H
--- src/Public/Tools.h
+++
@@ -1,8 +0,0 @@
-#ifndef TOOLS_H
-#define TOOLS_H
-
-#include "WinTools.h"
-#include "LinuxTools.h"
-#include "MacTools.h"
-
-#endif
